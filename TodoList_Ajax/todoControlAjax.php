d709e26c132c17543f8d5a88117f8e6a781dcc24
108213050 <s108213050@mail1.ncnu.edu.tw>
2022-01-09 19:05:00 +0800

    Todo List HW

---
+++ TodoList_Ajax/todoControlAjax.php
@@ -0,0 +1,47 @@
+<?php
+require('todoModel.php');
+// 檢查有沒有這個act傳入值
+if (isset($_REQUEST['act'])){
+	$act=$_REQUEST['act'];
+}else{
+	$act = '';
+}
+
+switch ($act) {
+	case "addJob":
+		$title=$_POST['title'];
+		$note=$_POST['note'];
+		if ($title) {
+			//實質上insert的
+			addJob($title,$note);// 防呆
+		}
+		break;
+	case "setFinish":
+		// 傳回工作ID
+		// 強迫變成整數，做個簡單防呆
+		$id=(int)$_REQUEST['id'];
+		if ($id>0) {
+			// 傳給後台todoModel去處理工作完成的動作
+			setFinished($id);
+		}
+		break;
+	case "getList":
+		// 產生一個清單
+		// 假設沒有多個使用者，單一使用者
+		// $list : 未完成工作清單
+		// getjoblist() : 拿到清單
+		// 2: type = 2 ，未完成 
+		$id=(int)$_REQUEST['id'];
+		if ($id>= 0) {
+			$list = getJobList($id);
+			// setFinished($id);
+		}
+		// 將清單轉為json
+		// 給client端出裡
+		echo json_encode($list);
+		break;
+
+	default:
+}
+?>
+
