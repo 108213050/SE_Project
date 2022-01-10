a226579e7f2246b01c6eb7c46f1455f4145fd7cf
108213050 <s108213050@mail1.ncnu.edu.tw>
2022-01-10 21:45:41 +0800

    security

--- guestbook/loginCheck.php
+++ guestbook/loginCheck.php
@@ -6,9 +6,10 @@ $pwd = $_POST['pwd'];
 
 $userName = mysqli_real_escape_string($conn,$userName);
 $pwd = mysqli_real_escape_string($conn,$pwd);
-
-$sql = "SELECT password, id FROM user WHERE loginID='$userName' and `password`=password('$pwd')";
+// $sql = "SELECE password,id FROM user WHERE loginID=$userName and password = PASSWORD($pwd)";
+$sql = "SELECT `password`, `id` FROM `user` WHERE loginID='$userName' and `password`=password('$pwd')";
 if ($result = mysqli_query($conn,$sql)) {
+	echo "right";
 	if ($row=mysqli_fetch_assoc($result)) {
 			$_SESSION['uID'] = $row['id'];
 			//provide a link to the message list UI
@@ -17,5 +18,8 @@ if ($result = mysqli_query($conn,$sql)) {
 		//print error message
 			echo "Invalid Username or Password - Please try again <br />";
 	}
+}else{
+	echo " Connect error";
+	echo $userName."密碼".$pwd;
 }
 ?>
